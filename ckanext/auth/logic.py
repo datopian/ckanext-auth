4a03dfa7350cbc86d504fd1eac6186819ecb0e66
anuveyatsu <anuar.ustayev@gmail.com>
2020-04-13 12:25:54 +0600

    [bug fix][xs]: if user id isn't found in the db, it was causing 500 error.
    We should return generic error message instead.

--- ckanext/auth/logic.py
+++ ckanext/auth/logic.py
@@ -7,8 +7,18 @@ _check_access = logic.check_access
 
 def user_login(context, data_dict):
     # Adapted from  https://github.com/ckan/ckan/blob/master/ckan/views/user.py#L203-L211
+    generic_error_message = {
+        u'errors': {
+            u'auth': [_(u'Username or password entered was incorrect')]
+        },
+        u'error_summary': {_(u'auth'): _(u'Incorrect username or password')}
+    }
     model = context['model']
-    user = model.User.get(data_dict['id']).as_dict()
+    user = model.User.get(data_dict['id'])
+    if not user:
+        return generic_error_message
+
+    user = user.as_dict()
 
     if data_dict[u'password']:
         identity = {
@@ -20,11 +30,6 @@ def user_login(context, data_dict):
         authUser = auth.authenticate(context, identity)
 
         if authUser != user['name']:
-            return {
-                u'errors': {
-                    u'password': [_(u'Password entered was incorrect')]
-                },
-                u'error_summary': {_(u'Password'): _(u'incorrect password')}
-            }
+            return generic_error_message
         else:
             return user
