fff325e0238334b54f23a392bb0682a5abc3da3f
Demenech <joaommdemenech@gmail.com>
2024-01-08 11:05:11 -0300

    feat(auth,tests): add test for email sign in

--- ckanext/auth/tests/test_logic.py
+++ ckanext/auth/tests/test_logic.py
@@ -29,6 +29,28 @@ def test_login():
 
     assert login['name'] == userobj['name']
 
+@pytest.mark.usefixtures('with_plugins', 'test_request_context')
+def test_login_w_email():
+    userobj = factories.Sysadmin(password='testpass1234')
+    session = model.Session
+    context = {
+        'model': model,
+        'session': session,
+        'user': userobj['name'],
+        'ignore_auth': True,
+        'user_obj': userobj,
+    }
+
+    login = auth_logic.user_login(
+        context=context,
+        data_dict={
+            'id': userobj['email'],
+            'password': 'testpass1234'
+        }
+    )
+
+    assert login['name'] == userobj['name']
+
 
 @pytest.mark.usefixtures('with_plugins', 'test_request_context')
 def test_login_wrong_password():
